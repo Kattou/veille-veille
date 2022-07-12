99ef045c520fdcc5097dddb2de369ee6152bcc9a
Kattou <108345080+Kattou@users.noreply.github.com>
2022-07-12 19:57:12 +0000

    Add files via upload

---
+++ techno.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+     body{background-color: white;}
+     .titre{
+        background-color: salmon;
+        border: 2px solid black;
+     }
+    </style>
+  
+</head>
+<body>
+    
+    <head><center><font size="5"><h1 class="titre"> l’informatique en nuage</h1></font size="5"></center></head>
+    
+
+        <head><center><font size="5"><h3 style="color:blue ;"><P> QUEST CE QUE L'INFORMATIQUE EN NUAGE</h3></font size="5"></P></center></head>
+            <head>La transformation numérique aide les entreprises à gagner rapidement en efficacité, en agilité et en connectivité, car elles utilisent la technologie pour transformer leurs processus d’affaires en quelque chose de plus facile, de plus rapide, de plus sûr, de plus flexible et de plus rentable. La technologie de l’informatique en nuage est une pierre angulaire de la transformation numérique.
+            L’informatique en nuage offre aux entreprises des modèles pratiques pour accéder aux offres d’infrastructure, de plateforme et de logiciel sur la base d’un paiement à l’utilisation. Grâce à l’informatique en nuage, les entreprises libèrent du capital, rationalisent la maintenance informatique, modernisent et adaptent leurs approches commerciales, intègrent la sécurité et la flexibilité dans les services et les solutions, aident les clients de manières nouvelles et développent leur activité dans des conditions de marché en constante évolution.
+
+                </head>
+                <head>
+             
+                  <p> <center> <iframe width="500" height="200" src="https://www.youtube.com/embed/6bf3BI4X610" title="Gérald Fillion - L'a b c de l'informatique en nuage" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+                    </p>  </head >
+                    
+                
+                <head><center><font size="5"><h3 style="color:blue ;"><P> LES TYPES DE MODELES DE DEPLOYEMENT DE L'INFORMATIQUE EN NUAGE</h3></font size="5"></P></center></head>
+                    <head>
+<h3> Nuage privé</h3>
+Le modèle de nuage privé fait référence aux ressources de l’informatique en nuage utilisées exclusivement par une seule entreprise ou organisation. Un nuage privé peut être situé dans votre centre de données sur site ou être hébergé dans le nuage par un fournisseur de services.
+
+Consultez nos solutions de nuage privé
+
+<h3> Nuage public</h3>
+Lorsqu’une organisation déploie un modèle de nuage public, tout le matériel, les logiciels et l’infrastructure de soutien sont détenus et gérés par un fournisseur de services, qui est fourni exclusivement sur Internet. Vous accédez à ces services et gérez votre compte au moyen d’un navigateur Web.
+
+<h3> Nuage hybride</h3>
+Un modèle d’informatique en nuage hybride combine les nuages publics et privés pour partager des données et des applications. Les nuages hybrides connectent l’infrastructure et les applications entre les ressources basées sur le nuage et les ressources existantes qui ne sont pas situées dans le nuage. Cette approche offre à votre entreprise une plus grande flexibilité et des options de déploiement plus importantes.
+
+Consultez nos solutions hybrides et en nuage multiservice
+                        
+                        </head >
+                         
+                        
+                        
+  
+                       
+</body>
+</html>
